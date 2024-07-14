aac644f40f166a0194bd096a849b3a182d10c2d0
Adilson Hideki Yamaguchi <dixonsp@gmail.com>
2024-07-13 21:05:34 -0300

    atualizacao

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from Utils.WebScraping import WebScraping
 
 app = FastAPI()
 
-@app.get("/links")
-async def get_website_links():
-    website_content = WebScraping()
-    return website_content
+@app.get("/")
+def hello()
+    return {'message':'Ola mundo'}
